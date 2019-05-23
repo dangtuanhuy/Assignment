14632a877bc2348c09388555ea86a33e1a53e0ca
Huy Dang Tuan <huy.dang@orientsoftware.com>
2019-05-23 10:36:49 +0700

    source

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-coco-app';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    if (firedNumber % 2 === 0) {
+      this.evenNumbers.push(firedNumber);
+    } else {
+      this.oddNumbers.push(firedNumber);
+    }
+  }
+  // console.log(fireNumber);
 }
+
